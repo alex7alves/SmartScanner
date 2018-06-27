1845b09dc43a759a28c495389990799f557e5525
alex7alves <alex7alves10@hotmail.com>
2018-06-27 10:38:41 -0300

    Implementando código para mostrar imagem

--- SmartScanner/app/src/main/java/alex/alves/smartscanner/TelaImagem.java
+++ SmartScanner/app/src/main/java/alex/alves/smartscanner/TelaImagem.java
@@ -1,5 +1,8 @@
 package alex.alves.smartscanner;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -7,10 +10,14 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class TelaImagem extends AppCompatActivity {
 
+
+    ImageView mostrarFotos;
+    int codigoFoto=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,14 +33,34 @@ public class TelaImagem extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });*/
+        mostrarFotos = (ImageView) findViewById(R.id.imageView2);
 
     }
     public void fabAction( View v)
     {
         Toast.makeText(getBaseContext()," VocÊ clicou no botão flutuante ",Toast.LENGTH_LONG).show();
 
+        Intent captarFoto =  new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        codigoFoto=123;
+        startActivityForResult(Intent.createChooser(captarFoto, "Selecione uma imagem"), codigoFoto);
 
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == TelaImagem.RESULT_OK) {
 
+              if (requestCode == codigoFoto) {
 
+                  Uri imagemCaptada = data.getData();
+                  mostrarFotos.setImageBitmap(getBitmap(imagemCaptada));
+                  mostrarFotos.setScaleType(ImageView.ScaleType.FIT_XY);
+
+            }
+        }
+    }
+    public Bitmap getBitmap(Uri imagem)
+    {
+        //Bitmap bm = BitmapFactory.decodeResource(getApplicationContext().getResources(),R.drawable.olhos);
+        return BitmapFactory.decodeFile(String.valueOf(imagem));
     }
 }
